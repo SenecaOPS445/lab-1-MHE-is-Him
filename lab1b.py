5f5a541bc02ffea5af82ea6f3f1e33a41af400e5
Malik Hamilton-Edwards <mhamilton-edwards@myseneca.ca>
2025-01-17 16:57:10 -0500

    completed lab

---
+++ lab1b.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+'''Description: This script prints a message using a string object.'''
+
+name = 'Isaac'
+print('How old are you ' + name + '?')
---
+++ lab1c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+'''Description: This script prints a message using integer objects.'''
+
+name = 'Isaac'
+age = 72
+print(name + ' is ' + str(age) + ' years old!')
